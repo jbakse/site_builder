1ca11da71811336a3e78dc2c6ad5b3cf3d36ab91
Justin Bakse <justin@tsfim.com>
2021-08-31 15:25:03 -0500

    add i_am_justin

---
+++ docs/i_am_justin/main.js
@@ -0,0 +1,595 @@
+console.log("$", $);
+
+var slide_data = {
+  hello: {
+      backgrounds: [
+        "./images/window/jbakse00.jpg",
+        "./images/window/jbakse01.jpg",
+        "./images/window/jbakse02.jpg",
+        "./images/window/jbakse04.jpg",
+        "./images/window/jbakse06.jpg",
+        "./images/window/jbakse07.jpg",
+      ],
+      colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+  },
+
+  justin_bakse: {
+    backgrounds: [
+      "./images/justin_bakse/car_sleep.jpg",
+      "./images/justin_bakse/door.jpg",
+      "./images/justin_bakse/jump.jpg",
+      "./images/justin_bakse/paintball.jpg",
+    ],
+    colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  },
+
+  professor: {
+    backgrounds: [
+      "./images/colors/white.png",
+      "./images/colors/black.png",
+      "./images/colors/white.png",
+       "./images/colors/black.png",
+
+    ],
+    colors: ["gray", "gray", "black", "white"]
+  },
+
+  director: {
+    backgrounds: [
+        "./images/colors/white.png",
+        "./images/colors/black.png",
+      "./images/window/jbakse07.jpg",
+
+    ],
+    colors: ["red", "red", "black"]
+  },
+
+  kid: {
+    backgrounds: [
+      //"./images/justin_bakse/car_sleep.jpg",
+      "./images/justin_bakse/me4.jpg",
+      "./images/justin_bakse/me4.jpg",
+      "./images/justin_bakse/me8.png",
+      "./images/justin_bakse/me8.png",
+      "./images/justin_bakse/me8.png",
+
+    ],
+    colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  },
+
+  code_art: {
+    backgrounds: [],
+    colors: ["white", "#888"]
+
+  },
+
+  medium: {
+    backgrounds: [
+      "images/medium/mario.png",
+      "images/medium/nomans.jpg",
+      "images/medium/settlers.jpg",
+      "images/medium/tilt.png",
+
+    ],
+    colors: ["white"]
+
+  },
+
+  ideas: {
+    backgrounds: [
+      "images/medium/mario.png",
+      "images/medium/nomans.jpg",
+      "images/medium/settlers.jpg",
+      "images/medium/tilt.png",
+    ],
+    colors: ["white"]
+
+  },
+
+  i_code: {
+    backgrounds: [],
+    colors: ["white", "#888"]
+  },
+
+  predead: {
+    backgrounds: [
+      "images/dead/blocks.jpg",
+      "images/dead/pencils.jpg",
+      "images/dead/lego.jpg",
+      "images/dead/playdoh.jpg",
+      "images/dead/tinker.jpg",
+    ],
+    colors: ["rgba(255,255,255,.7)"]
+  },
+
+
+  dead: {
+    backgrounds: [
+    //   "images/dead/blocks.jpg",
+    //   "images/dead/pencils.jpg",
+    //   "images/dead/lego.jpg",
+    //   "images/dead/playdoh.jpg",
+    //   "images/dead/tinker.jpg",
+    "images/deadthings/castle.jpg",
+    "images/deadthings/drawing.jpg",
+    "images/deadthings/spaceship.jpg",
+
+    ],
+    colors: ["rgba(255,255,255,.7)"]
+  },
+
+  prealive: {
+    backgrounds: [
+      "images/alive/apple.jpg",
+      "images/alive/aztec.jpg",
+      "images/alive/combat.jpg",
+      "images/alive/hello.png",
+      "images/alive/timex.jpg",
+      "images/alive/ultima.jpg",
+    ],
+    colors: ["rgba(255,255,255,.5)"]
+  },
+
+  alive: {
+    backgrounds: [
+      "images/alive/apple.jpg",
+      "images/alive/aztec.jpg",
+      "images/alive/combat.jpg",
+      "images/alive/hello.png",
+      "images/alive/timex.jpg",
+      "images/alive/ultima.jpg",
+    ],
+    colors: ["rgba(255,255,255,.5)"]
+  },
+
+
+  materials: {
+    backgrounds: [
+      "images/alive/apple.jpg",
+      "images/dead/blocks.jpg",
+      "images/alive/aztec.jpg",
+      "images/dead/pencils.jpg",
+      "images/alive/combat.jpg",
+      "images/dead/lego.jpg",
+      "images/alive/hello.png",
+      "images/dead/playdoh.jpg",
+      "images/alive/timex.jpg",
+      "images/dead/tinker.jpg",
+      "images/alive/ultima.jpg",
+    ],
+    colors: ["white", "magenta", "cyan"]
+  },
+
+  analogy: {
+    backgrounds: [
+        "./images/window/jbakse07.jpg",
+
+    ],
+    // tint: "white",
+    colors: ["black"]
+  },
+
+  start: {
+    backgrounds: [
+      "./images/window/jbakse07.jpg",
+    ],
+    // tint: "white",
+    colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  },
+
+
+  things: {
+      backgrounds: [
+        "./images/combscript/grab_1.png",
+        "./images/vines/church_1.jpg",
+        "./images/combscript/rail.jpg",
+        "./images/combscript/grab_2.png",
+        "./images/kp/wall.png",
+        "./images/combscript/top.jpg",
+        "./images/planet5/piled.jpg",
+        "./images/vines/mess.jpg",
+        "./images/code/battle.png",
+        "./images/code/circuit.png",
+        "./images/code/curious_volume.jpg",
+        "./images/code/front.jpg",
+        "./images/code/markov_map_1.png",
+        "./images/code/markov_map_2.png",
+        "./images/code/multitouch.jpg",
+        "./images/code/vertical_car_close.jpg",
+        "./images/code/village_1.jpg",
+      ],
+      colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  },
+
+  // compform: {
+  //     backgrounds: [
+  //         "images/compform/cull_both_before.png",
+  //         "images/compform/cull_close_before.png",
+  //         "images/compform/cull_close_noise.png",
+  //         "images/compform/cull_far_before.png",
+  //         "images/compform/grid_cull_dithered.png",
+  //     ],
+  //     colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  // },
+
+  // combscript_reasons: {
+  //   backgrounds: [
+  //     "images/combscript/docs.png",
+  //     "images/combscript/grab_1.png",
+  //     "images/combscript/grab_2.png",
+  //     "images/combscript/rail.jpg",
+  //     "images/combscript/top.jpg",
+  //   ],
+  //   colors: ["white"]
+  // },
+
+  // combscript: {
+  //   backgrounds: [
+  //     "images/combscript/docs.png",
+  //     "images/combscript/grab_1.png",
+  //     "images/combscript/grab_2.png",
+  //     "images/combscript/rail.jpg",
+  //     "images/combscript/top.jpg",
+  //   ],
+  //   colors: ["white"]
+  // },
+
+//   combscript_questions:{
+//     backgrounds: [
+//       "images/combscript/docs.png",
+//       "images/combscript/grab_1.png",
+//       "images/combscript/grab_2.png",
+//       "images/combscript/rail.jpg",
+//       "images/combscript/top.jpg",
+//     ],
+//     colors: ["white", "magenta", "cyan"]
+//   },
+
+//   recognition_reasons: {
+//     backgrounds: ["images/colors/white.png"],
+//     colors: ["white"]
+//   },
+
+//   recognition: {
+//     backgrounds: [
+//       "images/recognition/erik_A.jpg",
+//       "images/recognition/erik_B.jpg",
+//       "images/recognition/heather_A.jpg",
+//       "images/recognition/heather_B.jpg",
+//       "images/recognition/jon_A.jpg",
+//       "images/recognition/jon_B.jpg",
+//       "images/recognition/kindra_A.jpg",
+//       "images/recognition/kindra_B.jpg",
+//     ],
+//     colors: ["white"]
+//   },
+
+//   recognition_questions: {
+//     backgrounds: [
+//       "images/recognition/erik_A.jpg",
+//       "images/recognition/erik_B.jpg",
+//       "images/recognition/heather_A.jpg",
+//       "images/recognition/heather_B.jpg",
+//       "images/recognition/jon_A.jpg",
+//       "images/recognition/jon_B.jpg",
+//       "images/recognition/kindra_A.jpg",
+//       "images/recognition/kindra_B.jpg",
+//     ],
+//     colors: ["white", "magenta", "cyan"]
+//   },
+
+//   kp_reasons: {
+//     backgrounds: ["images/colors/white.png"],
+//     colors: ["white"]
+//   },
+//   kp: {
+//     backgrounds: [
+//       "images/kp/ar.jpg",
+//       "images/kp/close_touch.png",
+//       "images/kp/front_touch.png",
+//       "images/kp/timeline.png",
+//       "images/kp/wall.png",
+//       "images/kp/zone_3.png",
+
+//     ],
+//     colors: ["white"]
+//   },
+
+//   kp_questions: {
+//     backgrounds: [
+//       "images/kp/ar.jpg",
+//       "images/kp/close_touch.png",
+//       "images/kp/front_touch.png",
+//       "images/kp/timeline.png",
+//       "images/kp/wall.png",
+//       "images/kp/zone_3.png",
+
+//     ],
+//     colors: ["white", "magenta", "cyan"]
+//   },
+
+//   vines_reasons: {
+//     backgrounds: ["images/colors/white.png"],
+//     colors: ["white"]
+//   },
+//   vines: {
+//     backgrounds: [
+//       "images/vines/exp.png",
+//       "images/vines/illustrator.png",
+//       "images/vines/mess.jpg",
+//       "images/vines/orna.png",
+//       "images/vines/side.png",
+//       "images/vines/stacked.jpg",
+//       "images/vines/tests.jpg",
+//       "images/vines/vines_director.jpg",
+
+//     ],
+//     colors: ["white"]
+//   },
+
+//   vines_questions: {
+//     backgrounds: [
+//       "images/vines/exp.png",
+//   "images/vines/illustrator.png",
+//   "images/vines/mess.jpg",
+//   "images/vines/orna.png",
+//   "images/vines/side.png",
+//   "images/vines/stacked.jpg",
+//   "images/vines/tests.jpg",
+//   "images/vines/vines_director.jpg",
+
+//     ],
+//     colors: ["white", "magenta", "cyan"]
+//   },
+
+
+//   planet5_reasons: {
+//     backgrounds: ["images/colors/white.png"],
+//     colors: ["white"]
+//   },
+//   planet5: {
+//     backgrounds: [
+//       "images/planet5/fallen.jpg",
+//       "images/planet5/falling.jpg",
+//       "images/planet5/gameplay.jpg",
+//       "images/planet5/grab.jpg",
+//       "images/planet5/liftoff.jpg",
+//       "images/planet5/piled.jpg",
+//       "images/planet5/planets.jpg",
+//       "images/planet5/rocketman.jpg",
+//       "images/planet5/selfie.jpg",
+//       "images/planet5/stars.jpg",
+//       "images/planet5/topview.jpg",
+//       "images/planet5/tourist.jpg",
+
+//     ],
+//     colors: ["white"]
+//   },
+
+//   planet5_questions: {
+//     backgrounds: [
+//       "images/planet5/fallen.jpg",
+//       "images/planet5/falling.jpg",
+//       "images/planet5/gameplay.jpg",
+//       "images/planet5/grab.jpg",
+//       "images/planet5/liftoff.jpg",
+//       "images/planet5/piled.jpg",
+//       "images/planet5/planets.jpg",
+//       "images/planet5/rocketman.jpg",
+//       "images/planet5/selfie.jpg",
+//       "images/planet5/stars.jpg",
+//       "images/planet5/topview.jpg",
+//       "images/planet5/tourist.jpg",
+
+//     ],
+//     colors: ["white", "magenta", "cyan"]
+// },
+
+  // off_cranbrook: {
+  //     backgrounds: [
+  //       "./images/cranbrook_background/campus_gate.jpg",
+  //       "./images/cranbrook_background/campus_museum.jpg",
+  //       "./images/cranbrook_background/cranbrook_ariel_1.png",
+  //       "./images/cranbrook_background/cranbrook_ariel_3.png",
+  //       "./images/cranbrook_background/history_design.jpg",
+  //       "./images/cranbrook_background/logo.jpg",
+  //       "./images/cranbrook_background/campus_library.jpg",
+  //       "./images/cranbrook_background/campus_pool.jpg",
+  //       "./images/cranbrook_background/cranbrook_ariel_2.png",
+  //       "./images/cranbrook_background/cranbrook_ariel_4.png",
+  //       "./images/cranbrook_background/history_gate.jpg"
+  //     ],
+  //     colors: ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+  // },
+
+
+
+
+
+
+
+
+
+
+
+};
+
+var current_slide = "hello";
+var step = 0;
+
+var current_background_image = false;
+var current_color = "black";
+
+// var current_background_image_index = 0;
+
+function preload() {  // preload() runs once
+  Object.keys(slide_data).forEach(function(key) {
+    preloadSlide(slide_data[key]);
+  });
+
+
+
+}
+
+function preloadSlide(s) {
+  s.backgroundImages = [];
+  s.backgrounds.forEach( function(file) {
+    s.backgroundImages.push (loadImage(file));
+  });
+}
+
+function setup() {
+
+  var canvas = createCanvas(1920, 1080);
+  canvas.parent('background');
+}
+
+function draw() {
+  if (frameCount % 20 === 0) {
+    step++;
+    current_background_image = pick(slide_data[current_slide].backgroundImages, step);
+    current_color = pick(slide_data[current_slide].colors, step, "red");
+    // var position = current_background_image_index++ % slide_data[current_slide].backgroundImages.length;
+    // current_background_image = slide_data[current_slide].backgroundImages[position];
+
+
+    if (current_background_image) {
+      image_fill(current_background_image, 0, 0, 1920, 1080);
+    } else {
+      background(0);
+    }
+
+    $("#"+current_slide).css("color", current_color);
+
+    var tint = slide_data[current_slide].tint || "rgba(0, 0, 0, .2)";
+    $("#tint").css("background-color", tint);
+    console.log(current_slide);
+  }
+  // if (frameCount % 20 === 0) {
+  //   var r = Math.floor(Math.random() * 255);
+  //   var g = Math.floor(Math.random() * 255);
+  //   var b = Math.floor(Math.random() * 255);
+  //
+  //   $("#slides").css("color", "rgb("+r+","+g+","+b+")");
+  //
+  //   // if ( slide_data[current_slide].colors.length ) {
+  //   //   var current_color = slide_data[current_slide].colors[random(slide_data[current_slide].colors.length)];
+  //   // }
+  // }
+
+}
+
+function pick(array, index, _default) {
+  if (!array || !array.length) {
+    return _default;
+  }
+  return array[index % array.length];
+}
+
+function image_fill(img, x, y, w, h) {
+  var target_aspect = w / h;
+  var source_aspect = img.width / img.height;
+  x = x + 0.5 * w;
+  y = y + 0.5 * h;
+
+  if (target_aspect > source_aspect) {
+    w = w;
+    h = w / source_aspect;
+  } else {
+    h = h;
+    w = h * source_aspect;
+  }
+
+  push();
+
+  imageMode(CENTER);
+  image(img, x, y, w, h);
+  pop();
+}
+
+
+$(document).scroll(function() {
+    var cutoff = $(window).scrollTop() + 40;
+    $('.slide').each(function() {
+        if ($(this).offset().top + $(this).height() > cutoff) {
+            current_slide = $(this).attr("id");
+            return false;
+        }
+    });
+});
+
+
+function isScrolledIntoView(elem)
+{
+	let docViewTop = $(window).scrollTop();
+	let docViewBottom = docViewTop + $(window).height();
+
+	let elemTop = $(elem).offset().top;
+	let elemBottom = elemTop + $(elem).height();
+
+	// return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+	return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+}
+
+function autoplayVideoPosters() {
+	// console.log("autoplay");
+	let videos = $("video");
+
+	// let playCount = 0;
+	// let pauseCount = 0;
+	videos.each(function(e){
+		if (isScrolledIntoView(this)) {
+			// playCount++;
+			this.play().catch( function(e) {
+				// console.log("caught", e);
+				// getting a "The play() request was interrupted by a call to pause()." exception on live server but not local
+				// flooding the console, but not impacting experience
+				// just throw this exception away.
+				// not sure if this is related to this problem:
+				// https://bugs.chromium.org/p/chromium/issues/detail?id=593273
+			});
+
+		} else {
+			// pauseCount++;
+			this.pause();
+			this.currentTime = 0;
+		}
+	});
+
+	// console.log("p/p", playCount, pauseCount);
+
+}
+
+$(window).on("scroll", autoplayVideoPosters);
+setInterval(autoplayVideoPosters, 1000);
+
+
+// var background = {};
+// background.count = 0;
+// background.tick = function() {
+//   this.count++;
+//   position = this.count % slide_data[current_slide].backgrounds.length;
+//   image_name = slide_data[current_slide].backgrounds[position];
+//   console.log("background tick " + image_name);
+//
+//
+//   $("#background").css("background-image", "url("+image_name+")");
+//   console.log($(".background").css("background-image"));
+// };
+//
+//
+// var tickIntervalID = window.setInterval(tick, 500);
+//
+//
+//
+// function tick() {
+//   // console.log("tick");
+//   background.tick();
+//   var r = Math.floor(Math.random() * 255);
+//   var g = Math.floor(Math.random() * 255);
+//   var b = Math.floor(Math.random() * 255);
+//
+//   $("#slides").css("color", "rgb("+r+","+g+","+b+")");
+// }
